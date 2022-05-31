4e1ea01519bc20c05b0398ba030874a8a4492b8f
Steve Boyd <emteknetnz@gmail.com>
2022-05-31 16:18:33 +1200

    MNT Add auto-tag workflow

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # GitHub Action - Run tests
 Run Silverstripe CI matrix tests
 
-See [gha-ci](https://github.com/silverstripe/gha-ci)
+Only intended to be used within [gha-ci](https://github.com/silverstripe/gha-ci). The inputs all come from the matrix generated as a part of that workflow.
--- behat.php
+++ behat.php
@@ -2,15 +2,19 @@
 // this script will update the modules behat.yml to work with headless chrome
 // uses behat_headless.yml as the main behat.yml file,
 // and adds in the 'suites' from the module behat.yml
-$a = trim(file_get_contents('__behat_headless.yml'));
-$b = file_get_contents('__behat.yml');
-preg_match("#(?s)  suites:(.+?)\n  [a-z]#", $b, $m);
-if (!$m) {
-    preg_match("#(?s)  suites: (.+?)$#", $b, $m);
+$behatHeadless = trim(file_get_contents('__behat_headless.yml'));
+$behat = file_get_contents('__behat.yml');
+// Match where suites are defined with additional configuration.
+// Matches everything after "suites" before the next property starts.
+preg_match("#  suites:(.+?)\n  [a-z]#s", $behat, $matches);
+if (!$matches) {
+    // Match where a suite name is given as a string or array inline with the suites key
+    // e.g. "suites: some-suite" or "suites: []"
+    preg_match("#  suites: (.+?)$#s", $behat, $matches);
 }
-if (!$m) {
+if (!$matches) {
     echo "Could not match suites in behat.yml, cannot run behat\n\n";
-    die;
+    exit(1);
 }
-$c = str_replace('suites: []', 'suites: ' . $m[1], $a);
-file_put_contents('__behat.yml', $c);
+$combinedBehat = str_replace('suites: []', 'suites: ' . $matches[1], $behatHeadless);
+file_put_contents('__behat.yml', $combinedBehat);
