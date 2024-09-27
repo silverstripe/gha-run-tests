9dfa357c5b1aa5efa0179b0514334bcfaddb252f
Guy Sartorelli <guy.sartorelli@gmail.com>
2024-09-27 12:01:36 +1200

    FIX Fix nvm installation

--- install-nvm.sh
+++ install-nvm.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh
+wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh
 php -r '
-    $hash = "dd4b116a7452fc3bb8c0e410ceac27e19b0ba0f900fe2f91818a95c12e92130fdfb8170fec170b9fb006d316f6386f2b";
+    $hash = "faff9a72a1c8a202d6ad9b79e124684a8b128a0b3f44bdff0898bb6f4ca18550178cc7d435cfe10b2cc37396075b338d";
     if (hash_file("sha384", "install.sh") === $hash) {
         echo "Installer verified";
     } else {
